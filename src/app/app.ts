05bc8d966b430fac82b78838fb4fa3d443364a4e
awenzelhuemer <andreas.wenzelhuemer@gmail.com>
2026-05-13 18:34:31 +0200

    improve everything

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App implements OnInit {
+  private readonly swUpdate = inject(SwUpdate);
+  private readonly snackBar = inject(MatSnackBar);
+
+  ngOnInit(): void {
+    if (!this.swUpdate.isEnabled) return;
+
+    this.swUpdate.versionUpdates
+      .pipe(filter((e): e is VersionReadyEvent => e.type === 'VERSION_READY'))
+      .subscribe(() => {
+        const snack = this.snackBar.open('A new version is available.', 'Update', {
+          duration: 0,
+        });
+        snack.onAction().subscribe(() => document.location.reload());
+      });
+  }
+}
