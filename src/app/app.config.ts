53b3ac7162f0380f974f1b01f7e09b869a896be4
awenzelhuemer <andreas.wenzelhuemer@gmail.com>
2026-05-19 22:32:31 +0200

    improve registration strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideAuth(() => getAuth()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
-      registrationStrategy: 'registerImmediately'
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
 };
